33b20336dc11f512226e061556a5f16a6b2752ac
Jeremy Jacque <jeremy.jacque@algolia.com>
2018-12-10 10:57:37 +0100

    chore(review_pull) action to review a given pull request

---
+++ actions/review_pull.py
@@ -0,0 +1,17 @@
+from lib.base import BaseGithubAction
+from lib.formatters import pull_to_dict
+
+__all__ = [
+    'ReviewPullAction'
+]
+
+
+class ReviewPullAction(BaseGithubAction):
+    def run(self, user, repo, pull_id, message):
+
+        user = self._client.get_user(user)
+        repo = user.get_repo(repo)
+        pull = repo.get_pull(pull_id)
+        pull.create_review(commit=repo.get_commit(pull.merge_commit_sha), body=str(message), event='APPROVE')
+        result = pull_to_dict(pull)
+        return result
