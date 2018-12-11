e1a01d2ee05bbfe3c20c81edca13337c3615d26d
Jeremy Jacque <jeremy.jacque@algolia.com>
2018-12-11 11:27:09 +0100

    chore(review_pull) Add the event parameter to allow the user to choose the review action to perform

--- actions/review_pull.py
+++ actions/review_pull.py
@@ -7,12 +7,12 @@ __all__ = [
 
 
 class ReviewPullAction(BaseGithubAction):
-    def run(self, user, repo, pull_id, message):
+    def run(self, user, repo, pull_id, message, event):
 
         user = self._client.get_user(user)
         repo = user.get_repo(repo)
         pull = repo.get_pull(pull_id)
         pull.create_review(commit=repo.get_commit(pull.merge_commit_sha),
-            body=str(message), event='APPROVE')
+            body=str(message), event=str(event))
         result = pull_to_dict(pull)
         return result
