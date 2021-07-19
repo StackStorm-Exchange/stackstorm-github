b335d47953cf324ffe3ba6848471537c432c90f2
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2021-07-20 01:12:22 +0900

    Fixed implementation to be compatible with PEP8

--- tests/test_action_create_file.py
+++ tests/test_action_create_file.py
@@ -26,8 +26,6 @@ class CreateFileTest(GitHubBaseActionTestCase):
         }
 
     def test_create_file(self):
-        mock_repo = Mock()
-
         def _side_effect(*args, **kwargs):
             self._test_data['content'] = kwargs['content']
             return {'commit': Mock()}
@@ -36,14 +34,12 @@ class CreateFileTest(GitHubBaseActionTestCase):
 
         with patch.object(Github, 'get_user', return_value=Mock()) as mock_user:
             mock_user.return_value.get_repo.return_value.create_file.side_effect = _side_effect
-            result = action.run(**self.action_params)
+            action.run(**self.action_params)
 
         # This chceks sending content value is expected value
         self.assertEqual(self._test_data['content'], self.action_params['content'])
 
     def test_create_file_with_encoding_param(self):
-        mock_repo = Mock()
-
         def _side_effect(*args, **kwargs):
             self._test_data['content'] = kwargs['content']
             return {'commit': Mock()}
@@ -52,7 +48,7 @@ class CreateFileTest(GitHubBaseActionTestCase):
 
         with patch.object(Github, 'get_user', return_value=Mock()) as mock_user:
             mock_user.return_value.get_repo.return_value.create_file.side_effect = _side_effect
-            result = action.run(**dict(self.action_params, **{'encoding': 'base64'}))
+            action.run(**dict(self.action_params, **{'encoding': 'base64'}))
 
         # This chceks sending content value is expected value
         self.assertEqual(self._test_data['content'],
--- tests/test_action_update_file.py
+++ tests/test_action_update_file.py
@@ -27,8 +27,6 @@ class UpdateFileTest(GitHubBaseActionTestCase):
         }
 
     def test_update_file(self):
-        mock_repo = Mock()
-
         def _side_effect(*args, **kwargs):
             self._test_data['content'] = kwargs['content']
             return {'commit': Mock()}
@@ -37,14 +35,12 @@ class UpdateFileTest(GitHubBaseActionTestCase):
 
         with patch.object(Github, 'get_user', return_value=Mock()) as mock_user:
             mock_user.return_value.get_repo.return_value.update_file.side_effect = _side_effect
-            result = action.run(**self.action_params)
+            action.run(**self.action_params)
 
         # This chceks sending content value is expected value
         self.assertEqual(self._test_data['content'], self.action_params['content'])
 
     def test_update_file_with_encoding_param(self):
-        mock_repo = Mock()
-
         def _side_effect(*args, **kwargs):
             self._test_data['content'] = kwargs['content']
             return {'commit': Mock()}
@@ -53,7 +49,7 @@ class UpdateFileTest(GitHubBaseActionTestCase):
 
         with patch.object(Github, 'get_user', return_value=Mock()) as mock_user:
             mock_user.return_value.get_repo.return_value.update_file.side_effect = _side_effect
-            result = action.run(**dict(self.action_params, **{'encoding': 'base64'}))
+            action.run(**dict(self.action_params, **{'encoding': 'base64'}))
 
         # This chceks sending content value is expected value
         self.assertEqual(self._test_data['content'],
