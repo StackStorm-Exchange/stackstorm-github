5bc19a1f84363edb48ba9f6813b25017741a6cc3
MarlonHeiber <marlon.heiber@stone.com.br>
2022-07-19 17:00:06 -0300

    pylint

--- actions/create_file.py
+++ actions/create_file.py
@@ -2,50 +2,69 @@ from lib.base import BaseGithubAction
 from lib.formatters import file_response_to_dict, decode_base64
 from lib.utils import prep_github_params_for_file_ops
 
-__all__ = [
-    'CreateFileAction'
-]
+__all__ = ["CreateFileAction"]
 
 
 class CreateFileAction(BaseGithubAction):
-    def run(self, user, repo, path, message, content, github_type, api_user,
-            branch=None, committer=None, author=None, encoding=None):
-
-        enterprise = self._is_enterprise(github_type)
-
-        if api_user:
-            self.token = self._get_user_token(api_user, enterprise)
-
+    def run(
+        self,
+        user,
+        repo,
+        path,
+        message,
+        content,
+        github_type,
+        api_user,
+        branch=None,
+        committer=None,
+        author=None,
+        encoding=None,
+    ):
         self._change_to_user_token_if_enterprise(api_user, github_type)
-        author, branch, committer = prep_github_params_for_file_ops(author,
-                                                                    branch,
-                                                                    committer)
+        author, branch, committer = prep_github_params_for_file_ops(
+            author, branch, committer
+        )
 
-        if encoding and encoding == 'base64':
+        if encoding and encoding == "base64":
             content = decode_base64(content)
 
         user = self._client.get_user(user)
         repo = user.get_repo(repo)
-        api_response = repo.create_file(path=path, message=message,
-                                        content=content, branch=branch,
-                                        committer=committer, author=author)
+        api_response = repo.create_file(
+            path=path,
+            message=message,
+            content=content,
+            branch=branch,
+            committer=committer,
+            author=author,
+        )
         result = file_response_to_dict(api_response)
         return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import os
-    GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
-    GITHUB_ORG = os.environ.get('GITHUB_ORG')
-    GITHUB_REPO = os.environ.get('GITHUB_REPO')
-    COMMITTER = os.environ.get('COMMITTER', None)
-    AUTHOR = os.environ.get('AUTHOR', None)
 
-    act = CreateFileAction(config={'token': GITHUB_TOKEN, 'github_type': 'online'})
-    res = act.run(user=GITHUB_ORG, repo=GITHUB_REPO, path='README5.md',
-                  message='Test commit',
-                  content='Super duper read me file, pushed from Stackstorm github pack!\n',
-                  branch='branch1', committer=COMMITTER, author=AUTHOR)
+    GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+    GITHUB_ORG = os.environ.get("GITHUB_ORG")
+    GITHUB_REPO = os.environ.get("GITHUB_REPO")
+    COMMITTER = os.environ.get("COMMITTER", None)
+    AUTHOR = os.environ.get("AUTHOR", None)
+
+    act = CreateFileAction(config={"token": GITHUB_TOKEN, "github_type": "online"})
+    res = act.run(
+        user=GITHUB_ORG,
+        repo=GITHUB_REPO,
+        path="README5.md",
+        message="Test commit",
+        content="Super duper read me file, pushed from Stackstorm github pack!\n",
+        branch="branch1",
+        committer=COMMITTER,
+        author=AUTHOR,
+        github_type="online",
+        api_user="api_user",
+    )
     import pprint
+
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(res)
--- actions/update_branch_protection.py
+++ actions/update_branch_protection.py
@@ -1,14 +1,20 @@
 from lib.base import BaseGithubAction
 from github.GithubObject import NotSet
 
-__all__ = [
-    'UpdateBranchProtectionAction'
-]
+__all__ = ["UpdateBranchProtectionAction"]
 
 
 class UpdateBranchProtectionAction(BaseGithubAction):
-    def run(self, user, repo, branch, required_status_checks, enforce_admins,
-            required_pull_request_reviews, restrictions):
+    def run(
+        self,
+        user,
+        repo,
+        branch,
+        required_status_checks,
+        enforce_admins,
+        required_pull_request_reviews,
+        restrictions,
+    ):
 
         user = self._client.get_user(user)
         repo = user.get_repo(repo)
@@ -18,8 +24,8 @@ class UpdateBranchProtectionAction(BaseGithubAction):
             strict = NotSet
             contexts = NotSet
         else:
-            strict = required_status_checks['strict']
-            contexts = required_status_checks['contexts']
+            strict = required_status_checks["strict"]
+            contexts = required_status_checks["contexts"]
 
         if not required_pull_request_reviews:
             dismissal_users = NotSet
@@ -28,53 +34,70 @@ class UpdateBranchProtectionAction(BaseGithubAction):
             require_code_owner_reviews = NotSet
             required_approving_review_count = NotSet
         else:
-            dismissal_users = required_pull_request_reviews['dismissal_users']
-            dismissal_teams = required_pull_request_reviews['dismissal_teams']
-            dismiss_stale_reviews = required_pull_request_reviews['dismiss_stale_reviews']
-            require_code_owner_reviews = required_pull_request_reviews['require_code_owner_reviews']
+            dismissal_users = required_pull_request_reviews["dismissal_users"]
+            dismissal_teams = required_pull_request_reviews["dismissal_teams"]
+            dismiss_stale_reviews = required_pull_request_reviews[
+                "dismiss_stale_reviews"
+            ]
+            require_code_owner_reviews = required_pull_request_reviews[
+                "require_code_owner_reviews"
+            ]
             required_approving_review_count = required_pull_request_reviews[
-                'required_approving_review_count']
+                "required_approving_review_count"
+            ]
 
         if not restrictions:
             user_push_restrictions = NotSet
             team_push_restrictions = NotSet
         else:
-            user_push_restrictions = restrictions['user_push_restrictions']
-            team_push_restrictions = restrictions['team_push_restrictions']
+            user_push_restrictions = restrictions["user_push_restrictions"]
+            team_push_restrictions = restrictions["team_push_restrictions"]
 
-        branch.edit_protection(strict=strict, contexts=contexts,
-                               enforce_admins=enforce_admins,
-                               dismissal_users=dismissal_users,
-                               dismissal_teams=dismissal_teams,
-                               dismiss_stale_reviews=dismiss_stale_reviews,
-                               require_code_owner_reviews=require_code_owner_reviews,
-                               required_approving_review_count=required_approving_review_count,
-                               user_push_restrictions=user_push_restrictions,
-                               team_push_restrictions=team_push_restrictions)
+        branch.edit_protection(
+            strict=strict,
+            contexts=contexts,
+            enforce_admins=enforce_admins,
+            dismissal_users=dismissal_users,
+            dismissal_teams=dismissal_teams,
+            dismiss_stale_reviews=dismiss_stale_reviews,
+            require_code_owner_reviews=require_code_owner_reviews,
+            required_approving_review_count=required_approving_review_count,
+            user_push_restrictions=user_push_restrictions,
+            team_push_restrictions=team_push_restrictions,
+        )
         return True
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import os
 
-    GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
-    GITHUB_ORG = os.environ.get('GITHUB_ORG')
-    GITHUB_REPO = os.environ.get('GITHUB_REPO')
-    GITHUB_BRANCH = os.environ.get('GITHUB_BRANCH')
+    GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+    GITHUB_ORG = os.environ.get("GITHUB_ORG")
+    GITHUB_REPO = os.environ.get("GITHUB_REPO")
+    GITHUB_BRANCH = os.environ.get("GITHUB_BRANCH")
 
     # As produced by get_branch_protection action
-    BRANCH_PROTECTION = {'enforce_admins': True,
-                         'required_pull_request_reviews': None,
-                         'required_status_checks': {'contexts': [], 'strict': True},
-                         'restrictions': None
-                         }
+    BRANCH_PROTECTION = {
+        "enforce_admins": True,
+        "required_pull_request_reviews": None,
+        "required_status_checks": {"contexts": [], "strict": True},
+        "restrictions": None,
+    }
 
-    act = UpdateBranchProtectionAction(config={'token': GITHUB_TOKEN, 'github_type': 'online'})
-    res = act.run(user=GITHUB_ORG, repo=GITHUB_REPO, branch=GITHUB_BRANCH,
-                  required_status_checks=BRANCH_PROTECTION['required_status_checks'],
-                  enforce_admins=BRANCH_PROTECTION['enforce_admins'],
-                  required_pull_request_reviews=BRANCH_PROTECTION['required_pull_request_reviews'],
-                  restrictions=BRANCH_PROTECTION['restrictions'])
+    act = UpdateBranchProtectionAction(
+        config={"token": GITHUB_TOKEN, "github_type": "online"}
+    )
+    res = act.run(
+        user=GITHUB_ORG,
+        repo=GITHUB_REPO,
+        branch=GITHUB_BRANCH,
+        required_status_checks=BRANCH_PROTECTION["required_status_checks"],
+        enforce_admins=BRANCH_PROTECTION["enforce_admins"],
+        required_pull_request_reviews=BRANCH_PROTECTION[
+            "required_pull_request_reviews"
+        ],
+        restrictions=BRANCH_PROTECTION["restrictions"],
+    )
     import pprint
 
     pp = pprint.PrettyPrinter(indent=4)
