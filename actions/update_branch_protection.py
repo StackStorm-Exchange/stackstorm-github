be2b512a556dfe9befacf123ba6bf7530e55898e
MarlonHeiber <marlon.heiber@stone.com.br>
2022-07-19 11:59:21 -0300

    variable not used

--- actions/update_branch_protection.py
+++ actions/update_branch_protection.py
@@ -8,9 +8,7 @@ __all__ = [
 
 class UpdateBranchProtectionAction(BaseGithubAction):
     def run(self, user, repo, branch, required_status_checks, enforce_admins,
-            required_pull_request_reviews, restrictions,
-            required_linear_history=False, allow_force_pushes=False,
-            allow_deletions=False):
+            required_pull_request_reviews, restrictions):
 
         user = self._client.get_user(user)
         repo = user.get_repo(repo)
